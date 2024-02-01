fae51e9837099307f306b370c5ca72008b692aca
Farhan <frashid2@ualberta.ca>
2024-01-31 19:20:38 -0700

    No idea

---
+++ app/src/main/java/com/example/cmput301w24_t59_lab4/Shape.java
@@ -0,0 +1,6 @@
+package com.example.cmput301w24_t59_lab4;
+
+public class Shape {
+    private int x;
+    private int y;
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CMPUT301W24-T59-lab4"
+include(":app")
+ 
