d9ed4eb36c63979986eff43c75b1502566f9bfdc
Farhan <frashid2@ualberta.ca>
2024-01-31 23:06:15 -0700

    final change in Shape.java

--- app/src/main/java/com/example/cmput301w24_t59_lab4/Shape.java
+++ app/src/main/java/com/example/cmput301w24_t59_lab4/Shape.java
@@ -3,9 +3,5 @@ package com.example.cmput301w24_t59_lab4;
 public class Shape {
     private int x;
     private int y;
-<<<<<<< HEAD
     private String color = "black";
-=======
-    private String color = "black";
->>>>>>> d9ef97d022b04f541a8f6c5e8cd4b1a3d5b550e1
 }
